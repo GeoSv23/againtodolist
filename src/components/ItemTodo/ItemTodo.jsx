d3f9d8f32a004b6405c4a1e7be73a6ca8a97b558
Георгий <georgii@Arinas-MacBook-Pro.local>
2024-04-22 09:14:28 +0300

    редактирование

--- src/components/ItemTodo/ItemTodo.jsx
+++ src/components/ItemTodo/ItemTodo.jsx
@@ -1,20 +1,17 @@
 import styles from "./ItemTodo.module.css";
 import { useMyStore } from "../../store/store";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 export const ItemTodo = () => {
   const checkTodo = useMyStore((state) => state.checkTodo);
   const filter = useMyStore((state) => state.filter);
   const { fetchTodos } = useMyStore();
-  const changeTodo = useMyStore(state => state.changeTodo)
-  const todoChanger = (id, newTitle) => {
+  const changeTodo = useMyStore((state) => state.changeTodo);
+  const todoChanger = (id, newTitle) => {};
 
-  }
-
-
-  const delTodo = useMyStore(state => state.delTodo)
+  const delTodo = useMyStore((state) => state.delTodo);
   const handleDelTodo = (id) => {
-    delTodo(id)
-  }
+    delTodo(id);
+  };
   const todos = useMyStore((state) => {
     switch (filter) {
       case "active":
@@ -25,28 +22,51 @@ export const ItemTodo = () => {
         return state.todos;
     }
   });
-  console.log(todos);
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+  //запрос к серверу
+  // useEffect(() => {
+  //   fetchTodos();
+  // }, []);
+  // const [value, setValue] = useState
+  const todoRef = useRef();
+  const handleTodoChange = (e, id) => {
+    const newTitle = e.target.value;
+    changeTodo(id, newTitle); // Обновляем заголовок задачи
+  };
+
+  // const todoOnFocus = (id) => {
+  //   if ()
+  //   todoRef.current.focus();
+  // };
   return (
     <>
       {todos.map((item) => (
         <div key={item.id} className={styles.ItemTodo}>
-          <input style={
-            {width: 30,
-            height:30,
-            cursor:"pointer"}
-          }
+          <input
+            style={{ width: 30, height: 30, cursor: "pointer" }}
             checked={item.completed}
             type="checkbox"
             onChange={() => checkTodo(item.id)}
           />
           {/* <span>{item.id}</span> */}
-          <h3 className={styles.itemTitle}>{item.title}</h3>
+          <input
+            ref={todoRef}
+            className={styles.itemTitle}
+            onChange={(e) => handleTodoChange(e, item.id)} // Вызываем функцию при изменении значения
+            value={item.title}
+          ></input>
           <div className={styles.btnsContainer}>
-            <button className={styles.ChangeBtn} onClick={() => todoChanger(item.id)}>Edit</button>
-            <button className={styles.DelBtn} onClick={()=>handleDelTodo(item.id)}>Delette</button>
+            <button
+              className={styles.ChangeBtn}
+              // onClick={() => todoOnFocus(item.id)}
+            >
+              Edit
+            </button>
+            <button
+              className={styles.DelBtn}
+              onClick={() => handleDelTodo(item.id)}
+            >
+              Delette
+            </button>
           </div>
         </div>
       ))}
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,44 +1,62 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+import { persist, devtools } from "zustand/middleware";
 
-export const useMyStore = create((set, get) => ({
-  todos: [],
-  addTodo: (title) => {
-    if (!title) {
-      return
-    }
-    const newTodo = {id: get().todos.length + 1 , title, completed: false}
-    set({todos: [newTodo, ...get().todos ]})
-  },
-  // changeTodo: (todoId, newTitle) => {
-  //   set({
-  //     todos: get().todos.map(todo =>
-  //       todoId === todo.id ? { ...todo, title: newTitle } : todo
-  //     )
-  //   });
-  // },
-  checkTodo: (todoId) => set({
-    todos: get().todos.map(todo =>
-      todoId === todo.id ? {...todo, completed: !todo.completed} : todo
-      )
-  }),
-  delTodo: (todoId) => set({
-    todos: get().todos.filter(todo => todo.id !== todoId)
-  }),
-  filter: 'all',
-  setFilter: (value) => set({filter: value})
-  ,
-  fetchTodos: async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-      if (!response.ok) {
-        throw new Error('Failed to fetch todos');
-      }
-      const data = await response.json();
-      data.length = 3
-      set({ todos: data });
-    } catch (error) {
-      console.error('Error fetching todos:', error);
-    }
-  }
+const storage = typeof window !== "undefined" ? localStorage : null;
 
-}));
+export const useMyStore = create(
+  devtools(
+    persist(
+      (set, get) => ({
+        todos: [],
+        addTodo: (title) => {
+          if (!title) {
+            return;
+          }
+          const newTodo = {
+            id: get().todos.length + 1,
+            title,
+            completed: false,
+          };
+          set({ todos: [newTodo, ...get().todos] });
+        },
+        changeTodo: (todoId, newTitle) => {
+          set({
+            todos: get().todos.map((todo) =>
+              todoId === todo.id ? { ...todo, title: newTitle } : todo
+            ),
+          });
+        },
+        checkTodo: (todoId) =>
+          set({
+            todos: get().todos.map((todo) =>
+              todoId === todo.id
+                ? { ...todo, completed: !todo.completed }
+                : todo
+            ),
+          }),
+        delTodo: (todoId) =>
+          set({
+            todos: get().todos.filter((todo) => todo.id !== todoId),
+          }),
+        filter: "all",
+        setFilter: (value) => set({ filter: value }),
+        fetchTodos: async () => {
+          try {
+            const response = await fetch(
+              "https://jsonplaceholder.typicode.com/todos"
+            );
+            if (!response.ok) {
+              throw new Error("Не удалось получить список задач");
+            }
+            const data = await response.json();
+            data.length = 3;
+            set({ todos: data });
+          } catch (error) {
+            console.error("Ошибка при получении списка задач:", error);
+          }
+        },
+      }),
+      { name: "myStore", getStorage: () => storage }
+    )
+  )
+);
